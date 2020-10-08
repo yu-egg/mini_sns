f44c13cd81c7bc67f41c1b1efb0273826fb6b20b
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-09 02:05:22 +0900

    Userモデルにログイン時の記述、

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,12 @@ class User < ApplicationRecord
     user = User.where(email: auth.info.email).first_or_initialize( #first_or_initializewhereメソッドとともに使うことで、whereで検索した条件のレコードがあればそのレコードのインスタンスを返し、なければ新しくインスタンスを作るメソッドです。
     nickname: auth.info.name,
       email: auth.info.email
-  )
+    )
+    if user.persisted? ## userが登録済みであるか判断を行い、「if文」の中が呼ばれます。新規登録時は、SnsCredentialモデルに保存されるタイミングで、user_idが確定していなかったので、SnsCredentialモデルとUserモデルは紐付いていません。ログインの際に、sns.userを更新して紐付けを行います。
+      sns.user = user
+      sns.save
+    end
+    user
   end
 end
 #first_or_createとfirst_or_initializeの違いは以下になります。
