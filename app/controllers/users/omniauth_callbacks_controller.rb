5c81939a127cd9a323a7419e4be20d003a0f3e13
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-09 01:05:16 +0900

    authorizationを定義しましょう。 authorizationはprivateメソッド内に記述します。合わせて、authorizationを呼び出す「facebook」と「google_oauth2」というアクションを定義しましょう。

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  def facebook
+    authorization
+  end
+  
+  def google_oauth2
+    authorization
+  end
+  
+  private
+  
+  def authorization
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+  end
+
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
